9884fdc1997f3670065a4888ba54343c3d4d61dd
Daniel Huynh <danielhuynh523@gmail.com>
2023-11-08 12:23:22 -0500

    fixed bugs and finished implementing the local storage functionality and page load

--- GameScript.js
+++ GameScript.js
@@ -1,15 +1,19 @@
 var word = null;
 var attempts = [];
 var correct = 0;
-var gamesPlayed = 0;
+var totalGuesses = 0;
 
 const messages = document.getElementById("messages");
 const currentCorrect = document.getElementById("currentCorrect");
 const gamesPlayedElement = document.getElementById("gamesPlayed");
+const totalGuessesElement = document.getElementById("totalGuesses");
+const avgGuessesElement = document.getElementById("avgGuesses");
 
 function loadedPage() {
-    if (localStorage.getItem("gamesPlayed") != null) {
-        gamesPlayed = localStorage.getItem("gamesPlayed");
+    console.log("loaded page");
+    if (localStorage.getItem("totalGuesses") != null) {
+        totalGuesses = localStorage.getItem("totalGuesses");
+        guessbox.removeAttribute("readonly");
     }
     if (localStorage.getItem("correct") != null) {
         correct = localStorage.getItem("correct");
@@ -19,53 +23,73 @@ function loadedPage() {
     }
 
     currentCorrect.innerHTML = correct;
-    gamesPlayedElement.innerHTML = gamesPlayed;
+    totalGuessesElement.innerHTML = totalGuesses;
+    if(correct == 0) {
+        avgGuessesElement.innerHTML = 0;
+    }
+    else {
+        avgGuessesElement.innerHTML = totalGuesses/correct;
+    }
 }
 
 function resetHistory() {
     localStorage.clear();
-    gamesPlayed = 0;
+    word = null;
+    guessbox.value = "";
     correct = 0;
+    totalGuesses = 0;
     attempts = [];
     currentCorrect.innerHTML = correct;
-    gamesPlayedElement.innerHTML = gamesPlayed;
+    totalGuessesElement.innerHTML = totalGuesses;
+    avgGuessesElement.innerHTML = 0;
+    guessbox.setAttribute("readonly", "readonly");
 }
 
 
 function setUpGame(newWord) {
     console.log("restarted");
-    if(gamesPlayed == 0) {
-        guessbox.removeAttribute("readonly");
-    }
+    console.log(newWord);
+    guessbox.removeAttribute("readonly");
     word = newWord;
     correct = 0;
     gamesPlayed = 0;
     attempts = [];
     guessbox.value = "";
     messages.innerHTML = "";
-    getRandomWord(setUpGame);
 }
 
 function newWord(newWord){
     word = newWord;
 }
 
-function setupMessage(){
-
+function setupMessage(attempt){
+    console.log(attempt);
 }
 
 function submitGuess(guess){
     var attempt={Status:"Correct!", Char:0, Loc:0, Length:"long"}
     guess = guess.toLowerCase();
+    totalGuesses++;
+    totalGuessesElement.innerHTML = totalGuesses;
+    localStorage.setItem("totalGuesses", totalGuesses);
     if(guess==word){
         alert("correct");
         setupMessage(attempt);
         getRandomWord(newWord);
         correct++;
         currentCorrect.innerHTML = correct;
+        localStorage.setItem("correct", correct);
     }
     else{
         alert("wrong");
+    }
+    attempts.push(attempt);
+    localStorage.setItem("attempts", attempts);
+    if(correct == 0) {
+        avgGuessesElement.innerHTML = 0;
+    }
+    else {
+        avgGuessesElement.innerHTML = totalGuesses/correct;
     }
 }
 
