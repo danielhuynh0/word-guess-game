d19cf847262e5447b41db556a3972eb6714eb561
Eric Li <lieric021@gmail.com>
2023-11-07 21:20:08 -0500

    game state

--- GameScript.js
+++ GameScript.js
@@ -1,5 +1,36 @@
-var word = 
+var word;
+var attempts = [];
+const messages = document.getElementById("messages");
 
-function newGame() {
-    alert("Button clicked!");
+function setupGame(newWord){
+    word = newWord;
+    attempts = [];
+
+    while (messages.firstChild) {
+        messages.removeChild(messages.firstChild);
+    }
 }
+
+function setUpMessage(attempt){
+
+}
+
+function submitGuess(guess){
+    guess = guess.toLowerCase();
+    if(guess)
+}
+
+function submitForm(){
+    const box = document.getElementById("guessbox");
+    const guess = box.value;
+
+    submitGuess(guess);
+}
+
+getRandomWord(setupGame);
+
+const newGameButton = document.getElementById("newGame");
+newGameButton.addEventListener("click", function() { getRandomWord(setupGame); });
+
+const form = document.getElementById("mainForm");
+form.addEventListener("submit", submitForm);
