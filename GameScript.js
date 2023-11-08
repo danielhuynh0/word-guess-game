9c9521a330371ab1fb29381ce0a4237ceb1e5373
Daniel Huynh <danielhuynh523@gmail.com>
2023-11-08 01:37:55 -0500

    implementing reset game and reset and saving history on load

--- GameScript.js
+++ GameScript.js
@@ -1,14 +1,49 @@
-var word;
+var word = null;
 var attempts = [];
+var correct = 0;
+var gamesPlayed = 0;
+
 const messages = document.getElementById("messages");
+const currentCorrect = document.getElementById("currentCorrect");
+const gamesPlayedElement = document.getElementById("gamesPlayed");
 
-function setupGame(newWord){
-    word = newWord;
-    attempts = [];
-
-    while (messages.firstChild) {
-        messages.removeChild(messages.firstChild);
+function loadedPage() {
+    if (localStorage.getItem("gamesPlayed") != null) {
+        gamesPlayed = localStorage.getItem("gamesPlayed");
     }
+    if (localStorage.getItem("correct") != null) {
+        correct = localStorage.getItem("correct");
+    }
+    if (localStorage.getItem("attempts") != null) {
+        attempts = localStorage.getItem("attempts");
+    }
+
+    currentCorrect.innerHTML = correct;
+    gamesPlayedElement.innerHTML = gamesPlayed;
+}
+
+function resetHistory() {
+    localStorage.clear();
+    gamesPlayed = 0;
+    correct = 0;
+    attempts = [];
+    currentCorrect.innerHTML = correct;
+    gamesPlayedElement.innerHTML = gamesPlayed;
+}
+
+
+function setUpGame(newWord) {
+    console.log("restarted");
+    if(gamesPlayed == 0) {
+        guessbox.removeAttribute("readonly");
+    }
+    word = newWord;
+    correct = 0;
+    gamesPlayed = 0;
+    attempts = [];
+    guessbox.value = "";
+    messages.innerHTML = "";
+    getRandomWord(setUpGame);
 }
 
 function newWord(newWord){
@@ -23,11 +58,14 @@ function submitGuess(guess){
     var attempt={Status:"Correct!", Char:0, Loc:0, Length:"long"}
     guess = guess.toLowerCase();
     if(guess==word){
+        alert("correct");
         setupMessage(attempt);
         getRandomWord(newWord);
+        correct++;
+        currentCorrect.innerHTML = correct;
     }
     else{
-        
+        alert("wrong");
     }
 }
 
@@ -38,10 +76,14 @@ function submitForm(){
     submitGuess(guess);
 }
 
-getRandomWord(setupGame);
 
 const newGameButton = document.getElementById("newGame");
-newGameButton.addEventListener("click", function() { getRandomWord(setupGame); });
+newGameButton.addEventListener("click", function() { getRandomWord(setUpGame);});
 
 const form = document.getElementById("mainForm");
 form.addEventListener("submit", submitForm);
+
+const guessbox = document.getElementById("guessbox");
+
+const resetButton = document.getElementById("resetHistory");
+resetButton.addEventListener("click", resetHistory);
